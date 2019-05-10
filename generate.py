6b5b47dc9d00aae999e8795e61514e0a47e69f2f
Tijme Gommers <t.gommers@outlook.com>
2019-05-10 23:08:26 +0200

    Added Python generation and validation scripts.

---
+++ generate.py
@@ -0,0 +1,56 @@
+import os
+import re
+import sys
+import glob
+import json
+import cairosvg
+import subprocess
+
+manifest_data = {}
+input_files = glob.glob("./vectors/*/*.svg")
+
+def guard(result):
+    if not result:
+        raise Exception("Guard failed!")
+
+for input_file in input_files:
+    filename = "".join(input_file.split("/")[-1:])[:-4]
+    directory = "/".join(input_file.split("/")[:-1]) + "/"
+    issuer = directory[10:-1]
+
+    output_directory = "./dist/" + issuer
+    output_file = output_directory + "/" + filename + ".png"
+
+    safe_output_directory = re.sub(r"[^A-z0-9-\./]+", "", output_directory)
+    safe_output_file = re.sub(r"[^A-z0-9-\./]+", "", output_file)
+    safe_input_file = re.sub(r"[^A-z0-9-\./]+", "", input_file)
+
+    try:
+        guard(len(safe_output_directory.split(".")) == 2)
+        guard(len(safe_output_directory.split("/")) == 3)
+        guard(len(safe_output_file.split(".")) == 3)
+        guard(len(safe_output_file.split("/")) == 4)
+        guard(len(safe_input_file.split(".")) == 3)
+        guard(len(safe_input_file.split("/")) == 4)
+
+        with open(directory + "information.json", "r") as information_handle:
+            information = json.loads(information_handle.read())
+
+            subprocess.check_call("mkdir -p " + safe_output_directory, shell=True)
+            subprocess.check_call("cairosvg " + safe_input_file + " -f png -W 200 -H 200 -o " + safe_output_file, shell=True)
+
+            if issuer not in manifest_data.keys():
+                manifest_data[issuer] = {
+                    "domain": information["domain"],
+                    "additional_search_terms": information["additional_search_terms"],
+                    "icons": []
+                }
+
+            manifest_data[issuer]["icons"].append(issuer + "/" + filename + ".png")
+    except:
+        continue
+
+with open("dist/manifest.json", "w") as manifest_file:
+    json.dump(manifest_data, manifest_file)
+
+print("Generation done!")
---
+++ validate.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import json
+import generate
+from PIL import Image
+
+def guard(result, message):
+    if not result:
+        sys.exit("Guard failed! " + message)
+
+with open("./dist/manifest.json", "r") as manifest_handle:
+    manifest = json.loads(manifest_handle.read())
+
+    for issuer, information in manifest.items():
+        print("Evaulating issuer " + issuer + "...")
+
+        guard("domain" in information.keys(), "Domain is required.")
+        guard(len(information["domain"].split(".")) == 2, "Domain must have a maximum of one [dot] (e.g. 'twitter.com').")
+        guard(len(information["domain"].split(".")[0]) > 1, "Domain must have characters before the [dot].")
+        guard(len(information["domain"].split(".")[1]) > 1, "Domain must have characters behind the [dot].")
+
+        if "additional_search_terms" in information.keys():
+            guard(len(information["additional_search_terms"]) <= 10, "A maximum of 10 additional search terms are allowed.")
+
+        guard("icons" in information.keys(), "At least one icon is required.")
+
+        for icon in information["icons"]:
+            print("Evaluating icon " + icon + "...")
+
+            guard(os.path.isfile("./dist/" + icon), "PNG distribution file does not exist.")
+
+            dist_size = os.path.getsize("./dist/" + icon)
+            guard(dist_size <= 10000, "PNG distribution icon must be smaller than 10KB.")
+
+            try:
+                dist_image = Image.open("./dist/" + icon)
+                dist_image.verify()
+            except:
+                guard(False, "PNG distribution icon is not a valid image.")
+
+            guard(dist_image.format == "PNG", "PNG distribution icon is not a PNG file.")
+
+print("Validation done, everything looks good!")
